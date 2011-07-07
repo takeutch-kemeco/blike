7585dccf1d854ca4f666052e219a9d3fa014a8e9
kemeco <takeutchkemeco@gmail.com>
2011-07-07 19:40:12 +0900

    キーコードの変換関数のバグを修正
    switch文にbreakが抜けてたという、とんでもないミスをしてた。
    なんでこれで今まで動いてたんだ？

--- lib_src/gtk/drvgtk_transrate_keycode.c
+++ lib_src/gtk/drvgtk_transrate_keycode.c
@@ -12,27 +12,26 @@ gint32 transrate_keycode_DrvGtkKey(struct DrvGtkKey* a)
 	
 	
 	gint32 k;
-	
+
 	// GtkのキーコードをBlikeのキーコードへ変換
 	// 該当しないキーコードはそのまま返す
 	switch(a->value) {
-	case GDK_KEY_Return:		k = KEY_ENTER;
-	case GDK_KEY_Escape:		k = KEY_ESC;
-	case GDK_KEY_BackSpace:		k = KEY_BACKSPACE;
-	case GDK_KEY_Tab:		k = KEY_TAB;
-	case GDK_KEY_Left:		k = KEY_LEFT;
-	case GDK_KEY_Right:		k = KEY_RIGHT;
-	case GDK_KEY_Up:		k = KEY_UP;
-	case GDK_KEY_Down:		k = KEY_DOWN;
-	case GDK_KEY_Insert:		k = KEY_INS;
-	case GDK_KEY_Delete:		k = KEY_DEL;
-	default:			k = a->value;
+	case GDK_KEY_Return:		k = KEY_ENTER;		break;
+	case GDK_KEY_Escape:		k = KEY_ESC;		break;
+	case GDK_KEY_BackSpace:		k = KEY_BACKSPACE;	break;
+	case GDK_KEY_Tab:		k = KEY_TAB;		break;
+	case GDK_KEY_Left:		k = KEY_LEFT;		break;
+	case GDK_KEY_Right:		k = KEY_RIGHT;		break;
+	case GDK_KEY_Up:		k = KEY_UP;		break;
+	case GDK_KEY_Down:		k = KEY_DOWN;		break;
+	case GDK_KEY_Insert:		k = KEY_INS;		break;
+	case GDK_KEY_Delete:		k = KEY_DEL;		break;
+	default:			k = a->value;		break;
 	}
 	
 	
 	// キーを離した瞬間は 4095 を返す
 	if(a->state == DrvGtkKeyState_release) {
-//		k += 100000000;
 		k = 0x0FFF;
 	}
 	
