fbd369284049419140c9702ca0b31439424f8859
kemeco <takeutchkemeco@gmail.com>
2011-07-12 02:16:23 +0900

    drvgtk_malloc_0_aligned16()の追加
    16byte境界で確保した領域を、0で初期化するmalloc

--- lib_src/gtk/drvgtk_malloc.c
+++ lib_src/gtk/drvgtk_malloc.c
@@ -1,31 +1,17 @@
-//#define __DEBUG__
-
 #include <glib.h>
 
-gpointer drvgtk_malloc_aligned16(gsize size)
+static gpointer __drvgtk_malloc_aligned16(guint8* org)
 {
-	guint8* org = g_malloc(size + 32);
 	guint32 org_address = (guint32)org;
 	guint32 diff_address = org_address % 16;
 
-#ifdef __DEBUG__
-	g_printf("org[%p], org_address[0x%x], diff_address[[%d]\n", org, org_address, diff_address);
-#endif // __DEBUG__
-	
-	
+
 	guint32* header = (guint32*)(org + diff_address);
 	*header = org_address;
 	
 	guint32* a = header + 4;        // header + 16byte
 
-#ifdef __DEBUG__
-	g_printf(
-		"header[%p], *header[0x%x], ret_address[%p], *ret_address[%d], ret_address%16[%d]\n\n", 
-		header, *header, a, *a, (*a) % 16
-	);
-#endif // __DEBUG__
-	
-	
+
 	guint32 a_address = (guint32)a;
 	
 	if((a_address % 16) !=0) {
@@ -42,13 +28,23 @@ gpointer drvgtk_malloc_aligned16(gsize size)
 	return (gpointer)a;
 }
 
+gpointer drvgtk_malloc_aligned16(gsize size)
+{
+	guint8* org = g_malloc(size + 64);
+
+	return __drvgtk_malloc_aligned16(org);
+}
+
+gpointer drvgtk_malloc_0_aligned16(gsize size)
+{
+	guint8* org = g_malloc0(size + 64);
+
+	return __drvgtk_malloc_aligned16(org);
+}
+
 void drvgtk_free_aligned16(gpointer a)
 {       
 	guint32* header = ((guint32*)a) - 4;
 	guint32 org_address = *header;  
 	gpointer org = (guint32*)org_address;
-
-#ifdef __DEBUG__
-	g_printf("header[%p], org_address[%d], org=[%p]\n", header, org_address, org);
-#endif // __DEBUG__
 }
--- lib_src/gtk/drvgtk_malloc.h
+++ lib_src/gtk/drvgtk_malloc.h
@@ -4,6 +4,7 @@
 #define __DRVGTK_MALLOC_H__
 
 extern gpointer drvgtk_malloc_aligned16(gsize size);
+extern gpointer drvgtk_malloc_0_aligned16(gsize size);
 extern void drvgtk_free_aligned16(gpointer a);
 
 #endif // __DRVGTK_MALLOC_H__
