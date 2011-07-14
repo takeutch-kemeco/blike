f6959f9f9793561777727ba000c1ebe08a92e26f
kemeco <takeutchkemeco@gmail.com>
2011-07-14 22:52:39 +0900

    bl3d_io_macro.hのマクロ関数にて、関数の名前を修正。
    マクロ関数なのに、全て大文字ではない関数名を設定してしまってた。
    これを全て大文字の関数名へと修正。
    
    単なる趣味の問題

--- lib_src/bl3d/bl3d_io_macro.h
+++ lib_src/bl3d/bl3d_io_macro.h
@@ -1,3 +1,3 @@
-#define bl3d_setPix(x, y, c) {bl_setPix((x), (y), (c));}
+#define BL3D_SET_PIX(x, y, c) {bl_setPix((x), (y), (c));}
 
-#define bl3d_getPix(x, y, c, vram) {bl_slctWin((vram)); (c) = bl_getPix((x), (y)); bl_slctWin(0);}
+#define BL3D_GET_PIX(x, y, c, vram) {bl_slctWin((vram)); (c) = bl_getPix((x), (y)); bl_slctWin(0);}
