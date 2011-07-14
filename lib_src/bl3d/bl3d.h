13fa60697a497eae4bb36e56bbd8a8a960cf37a3
kemeco <takeutchkemeco@gmail.com>
2011-07-14 22:41:47 +0900

    bl3d_frame_buffer.c を追加。フレームバッファーをbl3dで持つことにした。
    このフレームバッファーは、pixelをfloat単位で扱うので、
    内部処理でのピクセル書き出しの際に、intへの変換が不要になる。
    
    最終的にblike側のフレームバッファーに書き出す際に、
    intへ変換することを想定している。
    また、この際に、blike側へはインターレースで書き出すことにより、
    処理時間を1/2にすることを想定してる。
    
    画面領域の周囲にあるバッファーへは、
    ポリゴンのピクセル描画時のif判定を、
    ポリゴンのデータ形式に制限を設けることで省略できると考えたので、
    それを想定して、スクリーンサイズの9倍の領域を確保してある。
    （
    　　　　三角形ポリゴンを構成する各辺の長さが、
    　　　　スクリーンの短辺よりも十分に短ければ、
    　　　　移動量を注意深く制限することで、if分を排除できると考えた。
    ）

--- lib_src/bl3d/bl3d.h
+++ lib_src/bl3d/bl3d.h
@@ -139,6 +139,35 @@ struct BL3D_FLAT_LIGHT {
 
 
 
+/// bl3d_frame_buffer.c
+
+struct BL3D_FRAME_BUFFER {
+	int 			frame_buffer_width;
+	int 			frame_buffer_height;
+	int			draw_offset_x0;
+	int			draw_offset_y0;
+	int			draw_width;
+	int			draw_height;
+	struct BL3D_CVECTOR** 	__y_offset_table;
+	struct BL3D_CVECTOR** 	y_offset_table;	
+	struct BL3D_CVECTOR* 	pixel;
+	int*			copy_buffer;
+} __attribute((aligned(16)));
+
+extern struct BL3D_FRAME_BUFFER* bl3d_new_frame_buffer(
+	const int width,
+	const int height,
+	const int offset_x0,
+	const int offset_y0,
+	const int draw_width,
+	const int draw_height
+);
+
+
+
+
+
+
 /// bl3d_math.c
 
 /// sin
