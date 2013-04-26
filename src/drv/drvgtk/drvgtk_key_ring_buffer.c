2ef3fcbfe51534189d45a7d0c7fa428bdad86ddb
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-04-26 20:52:41 +0900

    存在しない関数をヘッダーで宣言してたのを修正
    それと書式の些細な修正

--- src/drv/drvgtk/drvgtk_key_ring_buffer.c
+++ src/drv/drvgtk/drvgtk_key_ring_buffer.c
@@ -5,48 +5,47 @@
 #include "drvgtk_key_ring_buffer.h"
 #include "drvgtk_transrate_keycode.h"
 
-struct DrvGtkKeyRingBuffer* new_DrvGtkKeyRingBuffer(
-	gint32 key_len,
-	gint32* int_key,
-	gint32* read_index,
-	gint32* write_index,
-	gint32* key_count
-)
+struct DrvGtkKeyRingBuffer* new_DrvGtkKeyRingBuffer(gint32 key_len,
+                                                    gint32 *int_key,
+                                                    gint32 *read_index,
+                                                    gint32 *write_index,
+                                                    gint32 *key_count)
 {
-	struct DrvGtkKeyRingBuffer* a = g_malloc(sizeof(*a));
-	
-	a->key		= g_malloc(sizeof(*(a->key)) * key_len);
-	a->key_len	= key_len;
-	
-	a->int_key	= int_key;
-	a->read_index	= read_index;
-	a->write_index	= write_index;
-	a->key_count	= key_count;
-	
-	return a;
+        struct DrvGtkKeyRingBuffer *a = g_malloc(sizeof(*a));
+
+        a->key         = g_malloc(sizeof(*a->key) * key_len);
+        a->key_len     = key_len;
+
+        a->int_key     = int_key;
+        a->read_index  = read_index;
+        a->write_index = write_index;
+        a->key_count   = key_count;
+
+        return a;
 }
 
-void free_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a)
+void free_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer *a)
 {
-	g_free(a);
+        g_free(a);
 }
 
 extern void bl_putKeyB(int n, int *p);
 
-void write_c_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a, struct DrvGtkKey* key)
+void write_c_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer *a,
+                                 struct DrvGtkKey *key)
 {
-	gint32 c = transrate_keycode_DrvGtkKey(key);
+        gint32 c = transrate_keycode_DrvGtkKey(key);
 
-	switch(key->state) {
-	case DrvGtkKeyState_none:
-		break;
+        switch(key->state) {
+        case DrvGtkKeyState_none:
+                break;
 
-	case DrvGtkKeyState_press:
-		bl_putKeyB(1, &c);
-		break;
+        case DrvGtkKeyState_press:
+                bl_putKeyB(1, &c);
+                break;
 
-	case DrvGtkKeyState_release:
-		bl_putKeyB(1, &c);
-		break;
-	}
+        case DrvGtkKeyState_release:
+                bl_putKeyB(1, &c);
+                break;
+        }
 }
--- src/drv/drvgtk/drvgtk_key_ring_buffer.h
+++ src/drv/drvgtk/drvgtk_key_ring_buffer.h
@@ -4,40 +4,32 @@
 #define __DRVGTK_KEY_RING_BUFFER_H__
 
 enum DrvGtkKeyState {
-	DrvGtkKeyState_press	= 1,
-	DrvGtkKeyState_release	= 2,
-	DrvGtkKeyState_none	= 0,
+        DrvGtkKeyState_press   = 1,
+        DrvGtkKeyState_release = 2,
+        DrvGtkKeyState_none    = 0,
 };
 
 struct DrvGtkKey {
-	guint32			value;
-	enum DrvGtkKeyState	state;
+        guint32 value;
+        enum DrvGtkKeyState state;
 };
 
 struct DrvGtkKeyRingBuffer {
-	struct DrvGtkKey*	key;
-	gint32			key_len;
-	
-	gint32*			int_key;
-	gint32*			read_index;
-	gint32*			write_index;
-	gint32*			key_count;
+        struct DrvGtkKey *key;
+        gint32 key_len;
+        gint32 *int_key;
+        gint32 *read_index;
+        gint32 *write_index;
+        gint32 *key_count;
 };
 
-extern struct DrvGtkKeyRingBuffer* new_DrvGtkKeyRingBuffer(
-	gint32 key_len,
-	gint32* int_key,
-	gint32* read_index,
-	gint32* write_index,
-	gint32* key_count
-);
-extern void free_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a);
-
-extern void write_c_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a, struct DrvGtkKey* key);
-
-extern void read_c_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a, struct DrvGtkKey* key);
-extern void read_c_view_only_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a, struct DrvGtkKey* key);
-
-extern void clean_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer* a);
+struct DrvGtkKeyRingBuffer* new_DrvGtkKeyRingBuffer(gint32 key_len,
+                                                    gint32 *int_key,
+                                                    gint32 *read_index,
+                                                    gint32 *write_index,
+                                                    gint32 *key_count);
+void free_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer *a);
+void write_c_DrvGtkKeyRingBuffer(struct DrvGtkKeyRingBuffer *a,
+                                 struct DrvGtkKey *key);
 
 #endif // __DRVGTK_KEY_RING_BUFFER_H__
