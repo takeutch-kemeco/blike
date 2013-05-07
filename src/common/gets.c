c8523daa3048d06715724ca7665c3239ebf731c0
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-05-07 21:17:37 +0900

    bl_gets()がメモリー違反で動作しなかったバグを修正
    まだ残ってるバグ:
    カーソルキーを使ってウインドウ幅以上の長さの文字列を入力した場合に、
    カーソルの位置が変になる。

--- src/common/gets.c
+++ src/common/gets.c
@@ -1,52 +1,63 @@
 #include "blikedrv.h"
 
-#define w bl_work
+#define w       bl_work
 
 void bl_gets(char *s)
 /* せいぜい1024バイトしか入力できない, 改行は残る */
 {
-        int mod0 = w.mod, x0 = w.cx, c, l = 0, i;
+        int mod0 = w.mod, x0 = w.cx, y0 = w.cy, c, l = 0, i;
         bl_setMode(BL_PXOR);
-        s[0] = '¥0';
+        s[0] = '\0';
         for (;;) {
-                bl_fillRect(2, 16, w.cx * 8, w.cy * 16);
+
+                bl_fillRect(2, 16, w.cx * 8, w.cy * 8);
                 c = bl_inkey(BL_WAITKEY | BL_GETKEY | BL_DELFFF);
-                bl_fillRect(2, 16, w.cx * 8, w.cy * 16);
+                bl_fillRect(2, 16, w.cx * 8, w.cy * 8);
+
                 if (' ' <= c && c <= 0x7e && w.cx < w.csiz_x - 1) {
                         l++;
                         for (i = l; i > w.cx - x0; i--)
                                 s[i] = s[i - 1];
                         s[w.cx - x0] = c;
+
+                        w.cx++;
                 }
+
                 if (c == KEY_LEFT && w.cx > x0)
                         w.cx--;
+
                 if (c == KEY_RIGHT && w.cx - x0 < l)
                         w.cx++;
+
                 if (c == KEY_BACKSPACE && w.cx - x0 > 0) {
                         l--;
                         i = w.cx;
-                        bl_locate(x0 + l, w.cy);
+                        bl_locate(x0 + l, y0 / 2);
                         bl_putc(' ');
                         w.cx = i - 1;
-                //      w.cx--;
                         for (i = w.cx - x0; i <= l; i++)
                                 s[i] = s[i + 1];
                 }
+
                 if (c == KEY_DEL && w.cx - x0 < l && l > 0) {
                         l--;
                         i = w.cx;
-                        bl_locate(x0 + l, w.cy);
+                        bl_locate(x0 + l, y0 / 2);
                         bl_putc(' ');
                         w.cx = i;
                         for (i = w.cx - x0; i <= l; i++)
                                 s[i] = s[i + 1];
                 }
+
                 if (c == KEY_ENTER)
                         break;
-                bl_locate(x0, w.cy);
+
+                i = w.cx;
+                bl_locate(x0, y0 / 2);
                 bl_puts(s);
+                w.cx = i;
         }
         w.mod = mod0;
-        bl_putc('¥n');
+        bl_putc('\n');
         return;
 }
