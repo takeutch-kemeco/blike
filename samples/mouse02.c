c392010fd5a716cd234e700b0153cafbc743bd59
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-07-21 05:38:30 +0900

    ポインタデバイス利用のサンプルプログラムを追加
    mouse00.c ~ mouse02.c は、いずれも単純なペイントプログラム。
    
    mouse00.c は最も単純で、クリック時のポインタの位置へ点を打つ。
    
    mouse01.c は点ではなく、線を描く。
    
    mouse02.c はグラフィックタブレットの筆圧に応じて、線の濃さが変化する。
    （mouse02.c を試す場合はタブレットが必要）

---
+++ samples/mouse02.c
@@ -0,0 +1,103 @@
+#include <blike.h>
+
+typedef void (*bld_callback_motion_notify_MainWindow)(void* a,
+                                                      const double pos_x,
+                                                      const double pos_y,
+                                                      const double pressure,
+                                                      const double angle_x,
+                                                      const double angle_y);
+
+typedef void (*bld_callback_button_press_MainWindow)(void* a, const int button_number);
+typedef void (*bld_callback_button_release_MainWindow)(void* a, const int button_number);
+
+extern void __bld_set_callback_arg(void* a);
+extern void __bld_set_callback_motion_notify(bld_callback_motion_notify_MainWindow func);
+extern void __bld_set_callback_button_press(bld_callback_button_press_MainWindow func);
+extern void __bld_set_callback_button_release(bld_callback_button_release_MainWindow func);
+
+struct MyCallbackArg {
+        int press_flag;
+
+        double old_pos_x;
+        double old_pos_y;
+        double pos_x;
+        double pos_y;
+
+        double pressure;
+
+        double angle_x;
+        double angle_y;
+};
+
+static void my_motion_notify(void* __a,
+                             const double pos_x,
+                             const double pos_y,
+                             const double pressure,
+                             const double angle_x,
+                             const double angle_y)
+{
+        struct MyCallbackArg* a = __a;
+
+        a->old_pos_x = a->pos_x;
+        a->old_pos_y = a->pos_y;
+
+        a->pos_x = pos_x;
+        a->pos_y = pos_y;
+
+        a->pressure = pressure;
+
+        a->angle_x = angle_x;
+        a->angle_y = angle_y;
+
+        const int v = 255.0 * (1.0 - a->pressure);
+        const int col = bl_rgb(v, v, v);
+
+        switch (a->press_flag) {
+        case 1:
+                bl_setPix((int)(a->pos_x), (int)(a->pos_y), col);
+                a->press_flag = 2;
+                break;
+
+        case 2:
+                bl_setCol(col);
+                bl_drawLine((int)(a->old_pos_x), (int)(a->old_pos_y),
+                            (int)(a->pos_x), (int)(a->pos_y));
+
+                a->press_flag = 2;
+                break;
+        }
+}
+
+static void my_button_press(void* __a, const int button_number)
+{
+        struct MyCallbackArg* a = __a;
+
+        if (button_number & (1 << 0))
+                a->press_flag = 1;
+}
+
+static void my_button_release(void* __a, const int button_number)
+{
+        struct MyCallbackArg* a = __a;
+
+        if (button_number & (1 << 0))
+                a->press_flag = 0;
+}
+
+blMain()
+{
+        struct MyCallbackArg my_callback_arg = {0, 0, 0, 0, 0, 0,};
+        __bld_set_callback_arg(&my_callback_arg);
+        __bld_set_callback_motion_notify(my_motion_notify);
+        __bld_set_callback_button_press(my_button_press);
+        __bld_set_callback_button_release(my_button_release);
+
+        bl_openWin(400, 240);
+
+        bl_setCol(0xFFFFFF);
+        bl_fillRect(640, 480, 0, 0);
+
+        while(1) {
+                bl_wait(16);
+        }
+}
