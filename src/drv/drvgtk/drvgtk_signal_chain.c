0d14724c15cfde17138686d0f85e9d63000d88bc
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-08-23 07:41:36 +0900

    描画処理のリファクタリング

--- src/drv/drvgtk/drvgtk_signal_chain.c
+++ src/drv/drvgtk/drvgtk_signal_chain.c
@@ -47,37 +47,27 @@ static void show_window(struct DrvGtkPthreadData *a)
         show_MainWindow(a->main_window);
 }
 
-static inline void flash_line(guchar *dst, guint32 *src, gint i)
+static void flash_window(struct DrvGtkPthreadData *a,
+                         gpointer src_frame_buffer,
+                         gint x,
+                         gint y,
+                         gint width,
+                         gint height)
 {
-        while (i-->0) {
-                *dst++ = ((*src)>>16) & 0xFF;
-                *dst++ = ((*src)>>8 ) & 0xFF;
-                *dst++ = ((*src)>>0 ) & 0xFF;
-
-                src++;
+        void round_range(gint *a, gint min, gint max)
+        {
+                if (*a > max)
+                        *a = max;
+                else if (*a < min)
+                        *a = min;
         }
-}
 
-static void flash_window_whole_area(struct DrvGtkPthreadData *a,
-                                    gpointer src_frame_buffer)
-{
-        guint32 *src = (guint32*)src_frame_buffer;
-        guchar *dst = (guchar*)a->main_window->frame_buffer;
+        round_range(&x, 0, a->main_window->frame_buffer_width - 1);
+        round_range(&y, 0, a->main_window->frame_buffer_height - 1);
 
-        const guint area_len =
-                a->main_window->frame_buffer_width * a->main_window->frame_buffer_height;
+        round_range(&width, 0, a->main_window->frame_buffer_width - x);
+        round_range(&height, 0, a->main_window->frame_buffer_height - y);
 
-        if(src != NULL)
-                flash_line(dst, src, area_len);
-}
-
-static void flash_window_part_area(struct DrvGtkPthreadData *a,
-                                   gpointer src_frame_buffer,
-                                   const gint x,
-                                   const gint y,
-                                   const gint width,
-                                   const gint height)
-{
         const guint32 src_top_ofst = (y * a->main_window->frame_buffer_width) + x;
         const guint32 dst_top_ofst = src_top_ofst * 3;
 
@@ -87,48 +77,26 @@ static void flash_window_part_area(struct DrvGtkPthreadData *a,
         const guint32 src_next_ofst = a->main_window->frame_buffer_width;
         const guint32 dst_next_ofst = src_next_ofst * 3;
 
-        const guint line_len = width;
-
         if(src == NULL)
                 return;
 
-        int j;
-        for (j = 0; j < height; j++) {
-                flash_line(dst, src, line_len);
+        int j = height;
+        while (j-->0) {
+                guint32 *s = src;
+                guchar  *d = dst;
+                int i = width;
+                while (i-->0) {
+                        d[0] = (*s) >> 16;
+                        d[1] = (*s) >> 8;
+                        d[2] = (*s) >> 0;
+
+                        d += 3;
+                        s++;
+                }
 
                 src += src_next_ofst;
                 dst += dst_next_ofst;
         }
-}
-
-static inline void round_range(gint *a, const gint min, const gint max)
-{
-        if (*a > max)
-                *a = max;
-        else if (*a < min)
-                *a = min;
-}
-
-static void flash_window(struct DrvGtkPthreadData *a,
-                         gpointer src_frame_buffer,
-                         gint x,
-                         gint y,
-                         gint width,
-                         gint height)
-{
-        round_range(&x, 0, a->main_window->frame_buffer_width - 1);
-        round_range(&y, 0, a->main_window->frame_buffer_height - 1);
-
-        round_range(&width, 0, a->main_window->frame_buffer_width - x);
-        round_range(&height, 0, a->main_window->frame_buffer_height - y);
-
-        if (x == 0 &&
-            y == 0 &&
-            width == a->main_window->frame_buffer_width &&
-            height == a->main_window->frame_buffer_height)
-                flash_window_whole_area(a, src_frame_buffer);
-        else
-                flash_window_part_area(a, src_frame_buffer, x, y, width, height);
 
         redraw_MainWindow(a->main_window);
 }
