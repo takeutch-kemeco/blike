6b7a2080b7173ee30e582b7906a6c9320aded665
Takeutch Kemeco <takeutchkemeco@gmail.com>
2022-05-21 19:45:51 +0900

    drvgtk_signal_chain.c リファクタリング
    冗長な関数を削除

--- src/drv/drvgtk/drvgtk_signal_chain.c
+++ src/drv/drvgtk/drvgtk_signal_chain.c
@@ -36,59 +36,32 @@
 #include "drvgtk_pthread.h"
 #include "drvgtk_signal_chain.h"
 
-static void resize_window(struct DrvGtkPthreadData *a, gint width, gint height)
-{
-        g_signal_emit_by_name(a->main_window->wgt, "drvgtk-resize-window", width, height);
-}
-
-static void show_window(struct DrvGtkPthreadData *a)
-{
-        g_signal_emit_by_name(a->main_window->wgt, "drvgtk-show-window");
-}
-
-static void flash_window(struct DrvGtkPthreadData *a,
-                         gpointer src_frame_buffer,
-                         gint x,
-                         gint y,
-                         gint width,
-                         gint height)
-{
-        g_signal_emit_by_name(a->main_window->wgt, "drvgtk-flash-window", x, y, width, height);
-}
-
-static void exit_window(struct DrvGtkPthreadData *a)
-{
-        g_signal_emit_by_name(a->main_window->wgt, "drvgtk-exit-window");
-}
-
 gboolean update_DrvGtkSignalChain(gpointer data)
 {
         struct DrvGtkPthreadData *a = (struct DrvGtkPthreadData*)data;
+        struct MainWindow *m = a->main_window;
 
         (*(a->time_count)) += DRVGTK_SYGNAL_CHECK_INTERVAL;
 
         if(a->signal->resize_window.ready == TRUE) {
-                resize_window(a, a->signal->resize_window.width, a->signal->resize_window.height);
+                struct DrvGtkSignal_resize_window *s = &a->signal->resize_window;
+                g_signal_emit_by_name(m->wgt, "drvgtk-resize-window",s->width, s->height);
                 a->signal->resize_window.ready = FALSE;
         }
 
         if(a->signal->show_window.ready == TRUE) {
-                show_window(a);
+                g_signal_emit_by_name(m->wgt, "drvgtk-show-window");
                 a->signal->show_window.ready = FALSE;
         }
 
         if(a->signal->flash_window.ready == TRUE) {
-                flash_window(a,
-                             a->signal->flash_window.src_frame_buffer,
-                             a->signal->flash_window.x,
-                             a->signal->flash_window.y,
-                             a->signal->flash_window.width,
-                             a->signal->flash_window.height);
+                struct DrvGtkSignal_flash_window *s = &a->signal->flash_window;
+                g_signal_emit_by_name(m->wgt, "drvgtk-flash-window", s->x, s->y, s->width, s->height);
                 a->signal->flash_window.ready = FALSE;
         }
 
         if(a->signal->exit_window.ready == TRUE) {
-                exit_window(a);
+                g_signal_emit_by_name(m->wgt, "drvgtk-exit-window");
                 a->signal->exit_window.ready = FALSE;
         }
 
